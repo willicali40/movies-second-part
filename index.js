39f2ff4361081342bcfb3055ecc38e6decd41c80
William david <Willicali40@hotmail.com>
2023-04-18 16:02:52 -0500

    logic for infinite scroll done

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 let page = 1;
+let containerCard = "";
+let lastMovie;
 
 const API_KEY = "bd92795a62f0bc500027cd3dd29942a3";
 
+let options = {
+  rootMargin: "0px 0px 200px 0px",
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      page++;
+      fetchData();
+    }
+  });
+}, options);
+
 const fetchData = async () => {
   try {
-    const data = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&language=en-US&page=${page}`);
-    console.log(data);
+    const data = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&language=en-US&page=${page}`
+    );
     if (data.status === 200) {
       const dataJson = await data.json();
-      console.log(dataJson);
 
-      let containerCard = '';
-
-      dataJson.results.forEach(movie => {
+      dataJson.results.forEach((movie) => {
         containerCard += `
           <div class="container_cards-card">
               <img class="container_cards-img" src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Movie ${movie.title} Poster">
@@ -21,9 +35,20 @@ const fetchData = async () => {
         `;
       });
 
+      document.getElementById("container_cards").innerHTML = containerCard;
 
-      document.getElementById('container_cards').innerHTML = containerCard;
+      if (page < 1000) {
+        if (lastMovie) {
+          observer.unobserve(lastMovie);
+        }
 
+        const moviesShown = document.querySelectorAll(
+          ".container_cards .container_cards-card"
+        );
+        lastMovie = moviesShown[moviesShown.length - 1];
+
+        observer.observe(lastMovie);
+      }
     } else {
       console.log("error");
     }
@@ -33,4 +58,3 @@ const fetchData = async () => {
 };
 
 fetchData();
-
